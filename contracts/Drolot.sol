082d65ae16a72124a2b75df5a56dd0f93a898b68
remche <remche@remche.org>
2017-10-17 11:55:11 +0200

    first compiling version !

--- contracts/Drolot.sol
+++ contracts/Drolot.sol
@@ -3,17 +3,22 @@ pragma solidity ^0.4.4;
 contract Drolot {
 	address[] players;
 	address public owner;
+	uint amount = 1;
 
 	function Drolot() {
 		owner = msg.sender;
 	}
 
-	function () payable {
+	function dro() internal returns (address winner){
+			uint random = uint(sha3(block.timestamp))%10 +1;
+			winner = players[random];
+	}	
+
+	function () payable{
 		players.push(msg.sender);
 		if (players.length == 10){
-			uint random = uint(sha3(block.timestamp))%10 +1;
-			winner = address[random];
-			send();
+			address winner = dro();
+			winner.transfer(amount);
 		}
 	}
 }
